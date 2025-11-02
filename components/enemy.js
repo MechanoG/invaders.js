61ad4b5c1b17ece417ff6acfc0ca61bcfb86bfa9
MechanoG <glory_to_the_machine@protonmail.com>
2025-11-01 23:25:14 -0400

    adds sprites and map

--- components/enemy.js
+++ components/enemy.js
@@ -3,7 +3,25 @@ class enemy {
     constructor(scene){
 
         this.relatedScene = scene;
+    }
 
+    preload(){
+
+    }
+
+    create(){
+
+    }
+
+    attack1(){
+
+    }
+
+    attack2(){
+
+    }
+
+    attack3(){
 
     }
 
